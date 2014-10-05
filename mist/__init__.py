3c5bbb896d0d7bacde85a1159aa493367f4e874d
Technoboy10 <technoboy10@fake.email>
2014-10-05 14:59:43 -0600

    Refactored to use requests module instead

--- mist/__init__.py
+++ mist/__init__.py
@@ -1,6 +1,4 @@
-import urllib2 as u
-import urllib
-import json as j
+import requests
 class Bit:
     def __init__(self, auth_token, device_id):
         self.token = auth_token
@@ -8,6 +6,6 @@ class Bit:
         self.headers = {"Authorization": "Bearer " + self.token, "Accept": "application/vnd.littlebits.v2+json"}
     
     def output(self, pct, dur):
-        req = u.Request("https://api-http.littlebitscloud.cc/devices/" + self.id + "/output", urllib.urlencode({"percent": pct, "duration_ms": dur}), self.headers)
-        return u.urlopen(req).read()
+        r = requests.post("https://api-http.littlebitscloud.cc/devices/" + self.id + "/output", data={"percent": pct, "duration_ms": dur}, headers=self.headers)
+        return r.text
         
