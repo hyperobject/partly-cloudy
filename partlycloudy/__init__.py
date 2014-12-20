2a603fe387c3c933cbc0d43464fc4682756ba123
Technoboy10 <technoboy10@fake.email>
2014-12-19 17:24:03 -0700

    Added simple and raw input (thanks manitou48!)

--- partlycloudy/__init__.py
+++ partlycloudy/__init__.py
@@ -1,10 +1,22 @@
 import requests
+import re
+
 class Bit:
     def __init__(self, auth_token, device_id):
         self.token = auth_token
         self.id = device_id
         self.headers = {"Authorization": "Bearer " + self.token, "Accept": "application/vnd.littlebits.v2+json"}
-    
+
     def output(self, pct, dur):
         r = requests.post("https://api-http.littlebitscloud.cc/devices/" + self.id + "/output", data={"percent": pct, "duration_ms": dur}, headers=self.headers)
         return r.text
+
+    def iter_input(self):
+        #Use this for raw streaming input
+        r = requests.get("https://api-http.littlebitscloud.cc/devices/" + self.id + "/input", headers=self.headers, stream=True)
+        for line in r.iter_lines():
+            #if line: print line
+            yield line   # just fetch one line
+        return
+    def input(self):
+        return re.match(".*percent.:(\d+)",self.iter_input().next()).group(1)
