3530c94e16161bdedb94de7f0d7edbc768319262
Technoboy10 <technoboy10@fake.email>
2015-02-18 13:16:31 -0700

    Update to v3 url
    Also simplified the json getting stuff

--- partlycloudy/__init__.py
+++ partlycloudy/__init__.py
@@ -11,23 +11,37 @@ class Bit:
     def __init__(self, auth_token, device_id):
         self.token = auth_token
         self.id = device_id
-        self.headers = {"Authorization": "Bearer " + self.token, "Accept": "application/vnd.littlebits.v2+json"}
+        self.headers = {"Authorization": "Bearer " + self.token}
 
     def output(self, pct, dur):
-        r = requests.post("https://api-http.littlebitscloud.cc/devices/" + self.id + "/output", data={"percent": pct, "duration_ms": dur}, headers=self.headers)
+        r = requests.post("https://api-http.littlebitscloud.cc/v3/devices/" + self.id + "/output", data={"percent": pct, "duration_ms": dur}, headers=self.headers)
         return r.text
 
-    def raw_stream(self):
+    def json_stream(self):
         #Use this for raw streaming input
-        for line in requests.get("https://api-http.littlebitscloud.cc/devices/" + self.id + "/input", headers=self.headers, stream=True).iter_lines():
+        for line in requests.get("https://api-http.littlebitscloud.cc/v3/devices/" + self.id + "/input", headers=self.headers, stream=True).iter_lines():
             if line:
                 yield json.loads(line[5:])
 
+    def raw_stream(self):
+        #Use this for raw streaming input
+        for line in requests.get("https://api-http.littlebitscloud.cc/v3/devices/" + self.id + "/input", headers=self.headers, stream=True).iter_lines():
+            if line:
+                yield line
+
     def stream(self):
         #Use this for raw streaming input
-        for line in requests.get("https://api-http.littlebitscloud.cc/devices/" + self.id + "/input", headers=self.headers, stream=True).iter_lines():
+        for line in requests.get("https://api-http.littlebitscloud.cc/v3/devices/" + self.id + "/input", headers=self.headers, stream=True).iter_lines():
             if line:
-                yield int(json.loads(line[5:])["payload"]["percent"])
+                yield int(json.loads(line[5:])["percent"])
+
+    def overclock(self, interval):
+    	r = requests.put("https://api-http.littlebitscloud.cc/v3/devices/" + self.id, data={"input_interval_ms":interval}, headers=self.headers)
+    	return r.text
 
     def input(self):
+        #print "https://api-http.littlebitscloud.cc/devices/v3/" + self.id + "/input"
         return self.stream().next()
+
+    def url(self):
+    	return "https://api-http.littlebitscloud.cc/v3/devices/" + self.id
